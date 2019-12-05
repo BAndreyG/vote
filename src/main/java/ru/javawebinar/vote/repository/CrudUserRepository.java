93a40c9561e5f0c68e0064863985a68aa4218090
BAndreyG <wolf-irbis@yandex.ru>
2019-12-05 11:59:20 +1000

    crud repository ...

--- src/main/java/ru/javawebinar/vote/repository/CrudUserRepository.java
+++ src/main/java/ru/javawebinar/vote/repository/CrudUserRepository.java
@@ -1,8 +1,22 @@
 package ru.javawebinar.vote.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.vote.model.User;
 
+@Transactional(readOnly = true)
 public interface CrudUserRepository extends JpaRepository<User,Integer> {
+
+    public int deleteById(int id);
+
+    public User findById(int id);
+
+    /*@Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id=:id")
+    int delete(@Param("id") int id);*/
 
 }
--- src/main/java/ru/javawebinar/vote/repository/DataJpaUserRepository.java
+++ src/main/java/ru/javawebinar/vote/repository/DataJpaUserRepository.java
@@ -1,15 +1,31 @@
 package ru.javawebinar.vote.repository;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.vote.model.User;
 
 import java.util.List;
 
 @Repository
-public class DataJpaUserRepository  {
+public class DataJpaUserRepository {
+    @Autowired
     private CrudUserRepository repository;
-    public List<User> getAll(){
+
+    public List<User> getAll() {
         return repository.findAll();
     }
+
+    public boolean delete(int id) {
+        return repository.deleteById(id) != 0;
+    }
+
+    public User findId(int id) {
+        return repository.findById(id);
+    }
+
+    public User save(User user){
+        return repository.save(user);
+    }
+
 }
