874a798d627d07441a8becdd55985328f973bbe6
BAndreyG <wolf-irbis@yandex.ru>
2019-12-10 23:22:38 +1000

    fist_vote_v2

--- src/main/java/ru/javawebinar/vote/service/UserService.java
+++ src/main/java/ru/javawebinar/vote/service/UserService.java
@@ -1,10 +1,8 @@
 package ru.javawebinar.vote.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.support.CrudMethodMetadata;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.vote.repository.CrudUserRepository;
-import ru.javawebinar.vote.repository.DataJpaUserRepository;
 
 @Service
 public class UserService {
