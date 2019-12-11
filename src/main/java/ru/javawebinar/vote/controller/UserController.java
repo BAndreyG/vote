8990edeac4596a48ba771757132870e1538c548d
BAndreyG <wolf-irbis@yandex.ru>
2019-12-11 17:59:29 +1000

    controller ok, dont run app , insertable = false, updatable =false -- razobratca!!!

--- src/main/java/ru/javawebinar/vote/controller/UserController.java
+++ src/main/java/ru/javawebinar/vote/controller/UserController.java
@@ -5,15 +5,57 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.javawebinar.vote.model.User;
 import ru.javawebinar.vote.service.UserService;
+
+import java.util.List;
 
 //@RestController
 @Controller
-@RequestMapping(value ="api/v1/users")
+@RequestMapping(value = "api/v1/users")
 public class UserController {
 
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private UserService service;
+
+    public List<User> getAll() {
+        log.info("getAll");
+        return service.getAll();
+    }
+
+    public User get(int id) {
+        log.info("get {}", id);
+        return service.get(id);
+    }
+
+    public User create(User user) {
+        log.info("create {}", user);
+        if (user.isNew()) {
+            return service.create(user);
+        }
+        else throw new IllegalArgumentException(user + " must be new (id=null)");
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        service.delete(id);
+    }
+
+    public void update(User user, int id) {
+        log.info("update {} with id={}", user, id);
+       // assureIdConsistent(user, id);
+        service.update(user);
+    }
+
+    public User getByMail(String email) {
+        log.info("getByEmail {}", email);
+        return service.getByEmail(email);
+    }
+
+    /*public void enable(int id, boolean enabled) {
+        log.info(enabled ? "enable {}" : "disable {}", id);
+        service.enable(id, enabled);
+    }*/
 }
