edcfde5b6d134645bb401c5c5d84a99123aabb96
BAndreyG <wolf-irbis@yandex.ru>
2019-12-13 10:55:47 +1000

    app run. controller не возвращает данные

--- src/main/java/ru/javawebinar/vote/controller/UserController.java
+++ src/main/java/ru/javawebinar/vote/controller/UserController.java
@@ -3,15 +3,18 @@ package ru.javawebinar.vote.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.vote.model.User;
 import ru.javawebinar.vote.service.UserService;
 
+import java.net.URI;
 import java.util.List;
 
-//@RestController
-@Controller
+@RestController
+//@Controller
 @RequestMapping(value = "api/v1/users")
 public class UserController {
 
@@ -20,40 +23,61 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<User> getAll() {
-        log.info("getAll");
         return service.getAll();
     }
 
-    public User get(int id) {
+    @GetMapping("/{id}")
+    public User get(@PathVariable int id) {
         log.info("get {}", id);
         return service.get(id);
     }
 
-    public User create(User user) {
-        log.info("create {}", user);
-        if (user.isNew()) {
-            return service.create(user);
-        }
-        else throw new IllegalArgumentException(user + " must be new (id=null)");
-    }
-
-    public void delete(int id) {
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
         log.info("delete {}", id);
         service.delete(id);
     }
 
-    public void update(User user, int id) {
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void update(@RequestBody User user, @PathVariable int id) {
         log.info("update {} with id={}", user, id);
-       // assureIdConsistent(user, id);
+        // assureIdConsistent(user, id);
         service.update(user);
     }
+   /* @GetMapping
+    public List<User> getAll() {
+        log.info("getAll");
+        return service.findAll();
+    }
+*/
 
-    public User getByMail(String email) {
+/*
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createWithLocation(@RequestBody User user) {
+        log.info("create {}", user);
+        if (user.isNew()) {
+            User created = service.create(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+        }
+        else throw new IllegalArgumentException(user + " must be new (id=null)");
+    }
+
+
+
+    /
+
+   /* public User getByMail(String email) {
         log.info("getByEmail {}", email);
         return service.getByEmail(email);
     }
-
+*/
     /*public void enable(int id, boolean enabled) {
         log.info(enabled ? "enable {}" : "disable {}", id);
         service.enable(id, enabled);
