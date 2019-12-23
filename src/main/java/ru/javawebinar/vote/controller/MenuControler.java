70d35858d07a0f4de757b5668883e28cf215386f
BAndreyG <wolf-irbis@yandex.ru>
2019-12-24 00:06:40 +1000

    CRUD work -restotan/vote/user/menu vote LAZY + ONEtoMANY bad (menu)

---
+++ src/main/java/ru/javawebinar/vote/controller/MenuControler.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.vote.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.vote.model.Menu;
+import ru.javawebinar.vote.model.User;
+import ru.javawebinar.vote.service.MenuService;
+import ru.javawebinar.vote.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v1/menus",produces = MediaType.APPLICATION_JSON_VALUE)
+public class MenuControler {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private MenuService service;
+
+    @GetMapping()
+    public List<Menu> getAll() {
+        log.info("getALL {}");
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Menu get(@PathVariable int id){
+        log.info("get {}", id);
+        return service.get(id);
+    }
+
+}
