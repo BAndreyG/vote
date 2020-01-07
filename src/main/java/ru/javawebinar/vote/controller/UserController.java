a93bd58674346a9891736de27ab941851a7f72ea
BAndreyG <wolf-irbis@yandex.ru>
2020-01-07 22:15:06 +1000

    main controller next VoteController OK CRUD go...

--- src/main/java/ru/javawebinar/vote/controller/UserController.java
+++ src/main/java/ru/javawebinar/vote/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.vote.model.Role;
 import ru.javawebinar.vote.model.User;
 import ru.javawebinar.vote.service.UserService;
-import ru.javawebinar.vote.util.ValidationUtil;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -48,14 +47,48 @@ public class UserController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> createOrUpdate(@Valid User user,BindingResult result) { //, BindingResult result
-        if (result.hasErrors()) {
-            return ValidationUtil.getError(result);
-        }
+    public ResponseEntity<String> createOrUpdate(@Valid User user) {
         if (user.isNew()) {
             service.create(user);
         }
         else service.update(user, user.getId());
         return ResponseEntity.ok().build();
     }
+
+    /*@PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void update(@RequestBody User user, @PathVariable int id) {
+        log.info("update {} with id={}", user, id);
+        // assureIdConsistent(user, id);
+        service.update(user,id);
+    }*/
+
+
+/*
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createWithLocation(@RequestBody User user) {
+        log.info("create {}", user);
+        if (user.isNew()) {
+            User created = service.create(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+        }
+        else throw new IllegalArgumentException(user + " must be new (id=null)");
+    }
+
+
+
+    /
+
+   /* public User getByMail(String email) {
+        log.info("getByEmail {}", email);
+        return service.getByEmail(email);
+    }
+*/
+    /*public void enable(int id, boolean enabled) {
+        log.info(enabled ? "enable {}" : "disable {}", id);
+        service.enable(id, enabled);
+    }*/
 }
