28a0c13ac243cc30611412282449de2600a05c2c
BAndreyG <wolf-irbis@yandex.ru>
2020-01-08 21:46:19 +1000

    /login error
    main controller next
    VoteController OK
    CRUD go...

--- src/main/java/ru/javawebinar/vote/config/MvcConfig.java
+++ src/main/java/ru/javawebinar/vote/config/MvcConfig.java
@@ -9,6 +9,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        System.out.println("fing bag login");// TEST
         registry.addViewController("/login").setViewName("login");
 //        registry.addViewController("/news").setViewName("news");
     }
