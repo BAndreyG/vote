e7786f3976faa33c70e51e9751b85b5c6fbf33ed
BAndreyG <wolf-irbis@yandex.ru>
2020-01-09 12:29:36 +1000

    login form! permit OK
    secur go
    but Lazy not work

--- src/main/java/ru/javawebinar/vote/controller/UserController.java
+++ src/main/java/ru/javawebinar/vote/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.vote.model.Role;
 import ru.javawebinar.vote.model.User;
 import ru.javawebinar.vote.service.UserService;
+import ru.javawebinar.vote.util.ValidationUtil;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -47,51 +48,14 @@ public class UserController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> createOrUpdate(@Valid User user) { //, BindingResult result
-        /*if (result.hasErrors()) {
+    public ResponseEntity<String> createOrUpdate(@Valid User user,BindingResult result) { //, BindingResult result
+        if (result.hasErrors()) {
             return ValidationUtil.getError(result);
-        }*/
+        }
         if (user.isNew()) {
             service.create(user);
         }
         else service.update(user, user.getId());
         return ResponseEntity.ok().build();
     }
-
-    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void update(@RequestBody User user, @PathVariable int id) {
-        log.info("update {} with id={}", user, id);
-        // assureIdConsistent(user, id);
-        service.update(user,id);
-    }
-
-
-/*
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> createWithLocation(@RequestBody User user) {
-        log.info("create {}", user);
-        if (user.isNew()) {
-            User created = service.create(user);
-        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/{id}")
-                .buildAndExpand(created.getId()).toUri();
-        return ResponseEntity.created(uriOfNewResource).body(created);
-        }
-        else throw new IllegalArgumentException(user + " must be new (id=null)");
-    }
-
-
-
-    /
-
-   /* public User getByMail(String email) {
-        log.info("getByEmail {}", email);
-        return service.getByEmail(email);
-    }
-*/
-    /*public void enable(int id, boolean enabled) {
-        log.info(enabled ? "enable {}" : "disable {}", id);
-        service.enable(id, enabled);
-    }*/
 }
