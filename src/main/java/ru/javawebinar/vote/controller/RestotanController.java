a40d234fe90e32a5cfe785726067d0f8930dcf15
BAndreyG <wolf-irbis@yandex.ru>
2020-01-10 22:12:39 +1000

    RestotanController next.... RootController OK cRUD go...
    main controller next
    VoteController OK
    CRUD go...

--- src/main/java/ru/javawebinar/vote/controller/RestotanController.java
+++ src/main/java/ru/javawebinar/vote/controller/RestotanController.java
@@ -3,17 +3,22 @@ package ru.javawebinar.vote.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.vote.model.Menu;
 import ru.javawebinar.vote.model.Restoran;
+import ru.javawebinar.vote.repository.RestoranRepo;
 import ru.javawebinar.vote.service.RestoranService;
 
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Set;
+
+import static ru.javawebinar.vote.util.ValidationUtil.checkNotFoundWithId;
 
 @RestController
 @PreAuthorize("hasRole('ADMIN')")
@@ -23,23 +28,35 @@ public class RestotanController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
+    private RestoranRepo repo;
+
+    @Autowired
     private RestoranService service;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Restoran> getAll() {
         log.info("getAll restoran");
-        return service.getAll();
+        return repo.findAll(Sort.by("name"));
     }
 
     @GetMapping("/{id}")
     public Set<Menu> get(@PathVariable int id) {
-        log.info("get {}", id);
+        log.info("get {} restoran_id = ", id);
         return service.get(id);
     }
     @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
     public ResponseEntity<String> createOrUpdate(@Valid Restoran restoran){
         if (restoran.isNew()) service.create(restoran);
         else service.update(restoran);
         return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+        }
     }
 }
