ed73149b7c712f99e37d483136b03e349beb44b1
BAndreyG <wolf-irbis@yandex.ru>
2020-01-10 18:13:16 +1000

    query ...

--- src/main/java/ru/javawebinar/vote/repository/MenuRepo.java
+++ src/main/java/ru/javawebinar/vote/repository/MenuRepo.java
@@ -1,10 +1,17 @@
 package ru.javawebinar.vote.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.vote.model.Menu;
+
+import java.util.Set;
 
 @Repository
 public interface MenuRepo extends JpaRepository<Menu,Integer> {
     Menu findById(int id);
+
+    @Query("SELECT m FROM Menu m WHERE m.restoran_id=:id")
+    public Set<Menu> getMenus(@Param("id")int id);
 }
