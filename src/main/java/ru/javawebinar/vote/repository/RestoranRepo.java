37095a8b78dafc072a582469c0a606fa9797a8a8
BAndreyG <wolf-irbis@yandex.ru>
2020-01-10 18:13:16 +1000

    query ...

--- src/main/java/ru/javawebinar/vote/repository/RestoranRepo.java
+++ src/main/java/ru/javawebinar/vote/repository/RestoranRepo.java
@@ -1,11 +1,18 @@
 package ru.javawebinar.vote.repository;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.javawebinar.vote.model.Menu;
 import ru.javawebinar.vote.model.Restoran;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface RestoranRepo extends JpaRepository<Restoran,Integer> {
     Restoran getById(int id);
+
+//    @Query("SELECT m FROM Menu m WHERE m.restoran_id=:id")
+//    public Set<Menu> getMenus(@Param("id")int id);
 }
