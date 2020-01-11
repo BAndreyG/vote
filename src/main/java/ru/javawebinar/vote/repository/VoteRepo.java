07a686c96d5c6f779d1911184b0589930fca4bd5
BAndreyG <wolf-irbis@yandex.ru>
2020-01-11 23:03:56 +1000

    exception baddd curl ... encod good auth GOOD repository ok service ok COntrollerS ok
    main controller next
    VoteController OK
    CRUD go...

--- src/main/java/ru/javawebinar/vote/repository/VoteRepo.java
+++ src/main/java/ru/javawebinar/vote/repository/VoteRepo.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VoteRepo extends JpaRepository<Vote,Integer> {
-    Vote getById(int id);
 }
--- src/main/java/ru/javawebinar/vote/service/VoteService.java
+++ src/main/java/ru/javawebinar/vote/service/VoteService.java
@@ -1,28 +1,26 @@
 package ru.javawebinar.vote.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.javawebinar.vote.TO.ResTo;
-import ru.javawebinar.vote.TO.VoteTo;
+import org.springframework.util.Assert;
 import ru.javawebinar.vote.model.Menu;
 import ru.javawebinar.vote.model.Restoran;
 import ru.javawebinar.vote.model.User;
 import ru.javawebinar.vote.model.Vote;
 import ru.javawebinar.vote.repository.RestoranRepo;
 import ru.javawebinar.vote.repository.VoteRepo;
+import ru.javawebinar.vote.util.exception.NotFoundException;
 
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
+
+import static ru.javawebinar.vote.util.ValidationUtil.checkNotFoundWithId;
 
 @Service
 public class VoteService {
-
-    protected final Logger log = LoggerFactory.getLogger(getClass());
 
     private final VoteRepo repo;
     private final RestoranRepo repoRes;
@@ -34,28 +32,29 @@ public class VoteService {
     }
 
     public List<Restoran> getAll() {
-        return repoRes.findAll();
+        return repoRes.findAll(Sort.by("name"));
     }
-
-    public Restoran getTest(int id) {
-        return repoRes.getById(id);
-    }
-
 
     public Set<Menu> get(int id) {
-        log.info("get id =", id);
-        Set<Menu> menuSet = repoRes.getMenu(id);
-        return menuSet;
+        if (repoRes.existsById(id)){
+            repoRes.getMenu(id);
+        }
+        return  null;
+       // throw new NotFoundException("Restoran " + id + " is not found");
+        //throw new NotFoundException("Not found entity with " + msg);   return checkNotFoundWithId(
     }
 
     @Transactional
     public void create(User user, int restoran_id) {
+        Assert.notNull(user, "user must not be null");
         repoRes.sumVoteIncrement(restoran_id);
         repo.save(new Vote(user, restoran_id));
     }
 
     @Transactional
-    public void update(Vote vote,int restoran_id) {
-        repo.save(new Vote(vote.getUser(),restoran_id));
+    public void update(Vote vote, int restoran_id) {
+        repoRes.sumVoteDecrement(vote.getRestoran());
+        repoRes.sumVoteIncrement(restoran_id);
+        repo.save(new Vote(vote.getUser(), restoran_id));
     }
 }
