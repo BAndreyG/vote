5db99995d3e6e6316023af9c35280fef2b4cdc91
BAndreyG <wolf-irbis@yandex.ru>
2020-01-11 15:41:41 +1000

    UserController ok RestotanController ok RootController OK cRUD go...
    main controller next
    VoteController OK
    CRUD go...

--- src/main/java/ru/javawebinar/vote/service/UserService.java
+++ src/main/java/ru/javawebinar/vote/service/UserService.java
@@ -3,13 +3,17 @@ package ru.javawebinar.vote.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 import ru.javawebinar.vote.model.User;
 import ru.javawebinar.vote.repository.UserRepo;
 
 import java.util.List;
 
+import static ru.javawebinar.vote.util.ValidationUtil.checkNotFoundWithId;
+
 @Service
+
 public class UserService {
 
     private final UserRepo repository;
@@ -29,17 +33,18 @@ public class UserService {
     }
 
     public void delete(int id) {
-        repository.deleteById(id);
+        checkNotFoundWithId(repository.deleteById(id),id);
     }
 
     public User get(int id) {
-        return repository.getById(id);
+        return checkNotFoundWithId(repository.getById(id),id);
     }
 
     public List<User> getAll() {
         return repository.findAll();
     }
 
+    @Transactional
     public void update(User user,int id) {
         User userUpdate=new User(get(id));
         Assert.notNull(user, "user must not be null");
