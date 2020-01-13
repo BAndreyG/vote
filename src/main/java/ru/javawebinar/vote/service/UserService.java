43abbcafc10517c196a2cf6e8442fedabd46623c
BAndreyG <wolf-irbis@yandex.ru>
2020-01-13 17:48:18 +1000

    auth + curl 90%_

--- src/main/java/ru/javawebinar/vote/service/UserService.java
+++ src/main/java/ru/javawebinar/vote/service/UserService.java
@@ -24,7 +24,7 @@ public class UserService implements UserDetailsService {  //
     private final UserRepo repository;
     private final PasswordEncoder passwordEncoder;
 
-    /*@Autowired
+    /*@Autowired ///
     BCryptPasswordEncoder bCryptPasswordEncoder;
 */
     @Autowired
