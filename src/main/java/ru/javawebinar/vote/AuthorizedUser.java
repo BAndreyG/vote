591f3993c429992df215692c3aafea26141fd81e
BAndreyG <wolf-irbis@yandex.ru>
2020-01-15 23:28:15 +1000

    VoteService GOOD

--- src/main/java/ru/javawebinar/vote/AuthorizedUser.java
+++ src/main/java/ru/javawebinar/vote/AuthorizedUser.java
@@ -4,34 +4,14 @@ import ru.javawebinar.vote.model.User;
 
 public class AuthorizedUser extends org.springframework.security.core.userdetails.User {
     private static final long serialVersionUID = 1L;
-
-    //private UserTo userTo;
     private User user;
 
     public AuthorizedUser(User user) {
         super(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true, user.getRoles());
-        //this.userTo = UserUtil.asTo(user);
         this.user = user;
     }
 
-    /* public int getId() {
-         return userTo.id();
-     }
-
-     public void update(UserTo newTo) {
-         userTo = newTo;
-     }
-
-     public UserTo getUserTo() {
-         return userTo;
-     }
- */
     public User getUser() {
         return user;
     }
-
-   /* @Override
-    public String toString() {
-        return userTo.toString();
-    }*/
 }
